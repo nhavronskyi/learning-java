742a16c6159bf4c883608db36be359729bb2f197
Nazar Havronskyi <nhavronskyi@gmail.com>
2024-12-22 00:06:18 +0100

    Top-N finder changed to interface

--- mentoring/src/main/java/lesson_1/homework/tasks/TomNFinder.java
+++ mentoring/src/main/java/lesson_1/homework/tasks/TomNFinder.java
@@ -3,13 +3,13 @@ package lesson_1.homework.tasks;
 import java.util.Collection;
 import java.util.function.BiFunction;
 
-public class TomNFinder {
+public interface TomNFinder {
     /**
      * Top-N Finder: Find the top N elements in a collection dynamically.
      *
      * @return {@link Collection} of any top n elements
      */
-    public static BiFunction<Collection<?>, Integer, Collection<?>> getTopN() {
+    static BiFunction<Collection<?>, Integer, Collection<?>> getTopN() {
         return (collection, number) -> collection.stream()
                 .limit(number)
                 .toList();
