68404e994fa713eed8eea816e2b5e5a4669140b1
Nazar Havronskyi <nhavronskyi@gmail.com>
2024-12-26 17:53:06 +0100

    Dynamic Sorting Utility

--- mentoring/src/main/java/lesson_1/homework/Main.java
+++ mentoring/src/main/java/lesson_1/homework/Main.java
@@ -1,18 +1,37 @@
 package lesson_1.homework;
 
+import lesson_1.homework.tasks.DynamicSortingUtility;
 import lesson_1.homework.tasks.TomNFinder;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+
+import static lesson_1.homework.helpers.PersonGenerator.Person;
+import static lesson_1.homework.helpers.PersonGenerator.generatePerson;
 
 public class Main {
     public static void main(String[] args) {
-        // Top-N Finder
+        topNFinder();
+
+        dynamicSortingUtility();
+    }
+
+    private static void dynamicSortingUtility() {
+        DynamicSortingUtility.sort(
+                        List.of(1, generatePerson(2, "ann"), "temp", 3, 2, "a", generatePerson(1, "joe")),
+                        Map.of(Person.class, Comparator.comparing(Person::name))
+                )
+                .entrySet()
+                .forEach(System.out::println);
+    }
+
+    // Top-N Finder
+    private static void topNFinder() {
         TomNFinder.getTopN()
                 .apply(List.of(3, 2, 1), 2)
                 .forEach(System.out::println);
     }
-
-    // TODO Dynamic Sorting Utility: Create a utility that dynamically sorts a list of objects based on runtime-provided field names and directions.
 
     // TODO Dynamic Query Builder: Implement a dynamic SQL query builder using Generics. The builder should accept criteria and return a query string while ensuring type safety for criteria values.
 
