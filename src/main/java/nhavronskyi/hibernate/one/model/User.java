68474a75dfd0bca59f40eb184e94285eb43e1240
Nazar Havronskyi <nhavronskyi@gmail.com>
2025-05-11 12:02:23 +0200

    fixed: build

--- src/main/java/nhavronskyi/hibernate/one/model/User.java
+++ src/main/java/nhavronskyi/hibernate/one/model/User.java
@@ -4,11 +4,15 @@ package nhavronskyi.hibernate.one.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     long id;
---
+++ src/test/java/nhavronskyi/hibernate/one/repository/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package nhavronskyi.hibernate.one.repository;
+
+import nhavronskyi.hibernate.one.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    private static User getUser(int id, String test) {
+        return new User(id, test);
+    }
+
+    @Test
+    void shouldSelectAllUsers() {
+        List<User> testUsers = List.of(
+                getUser(1, "test"),
+                getUser(2, "test2"),
+                getUser(3, "test3")
+        );
+        Assertions.assertEquals(testUsers, userRepository.selectAllUsers());
+    }
+}
